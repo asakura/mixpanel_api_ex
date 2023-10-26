ebc636a5b8ec3e51329c2336f387e6d1d5422874
Mikalai Seva <mikalai.seva@gmail.com>
2023-10-26 18:15:56 +0200

    implement a plug for testing

---
+++ test/support/plug.ex
@@ -0,0 +1,34 @@
+defmodule MixpanelTest.Plug do
+  use Plug.Router
+
+  import Plug.Conn
+
+  plug(:match)
+  plug(:dispatch)
+
+  match _ do
+    {:ok, body, conn} = read_body(conn)
+    conn = fetch_query_params(conn)
+
+    response =
+      %{
+        request_url: request_url(conn),
+        http_protocol: get_http_protocol(conn) |> to_string(),
+        body: body,
+        method: conn.method,
+        headers: conn.req_headers,
+        # body_params: conn.body_params,
+        query_params: conn.query_params
+      }
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Jason.encode!(response))
+  end
+end
+
+defimpl Jason.Encoder, for: Tuple do
+  def encode(data, opts) when is_tuple(data) do
+    Jason.Encode.list(Tuple.to_list(data), opts)
+  end
+end
