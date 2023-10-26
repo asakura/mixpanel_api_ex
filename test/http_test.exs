a4896ec0e864fda9328ea15539c4842b4e2079df
Mikalai Seva <mikalai.seva@gmail.com>
2023-10-26 20:32:19 +0200

    adapt tests for HTTPC adapter

--- test/http_test.exs
+++ test/http_test.exs
@@ -21,54 +21,56 @@ defmodule MixpanelTest.HTTPTest do
   end
 
   describe "HTTPoison adapter" do
-    test "get/3" do
-      case Mixpanel.HTTP.Hackney.get("https://localhost:40010/get_endpoint", [], [:insecure]) do
-        {:ok, 200, _headers, body} ->
-          assert Jason.decode!(body)
-                 ~> %{
-                   "body" => "",
-                   "headers" =>
-                     in_any_order([["user-agent", "hackney/1.20.1"], ["host", "localhost:40010"]]),
-                   "http_protocol" => string(starts_with: "HTTP"),
-                   "method" => "GET",
-                   "query_params" => map(size: 0),
-                   "request_url" => string(ends_with: "/get_endpoint")
-                 }
+    for adapter <- [Mixpanel.HTTP.Hackney, Mixpanel.HTTP.HTTPC] do
+      test "#{adapter}.get/3" do
+        case unquote(adapter).get("https://localhost:40010/get_endpoint", [], insecure: true) do
+          {:ok, 200, _headers, body} ->
+            assert Jason.decode!(body)
+                   ~> %{
+                     "body" => "",
+                     "headers" =>
+                       list(min: 1, elements: list(min: 2, max: 2, elements: string())),
+                     "http_protocol" => string(starts_with: "HTTP"),
+                     "method" => "GET",
+                     "query_params" => map(size: 0),
+                     "request_url" => string(ends_with: "/get_endpoint")
+                   }
 
-        {:ok, status, _headers, _body} ->
-          refute "Expected 200, got #{status}"
+          {:ok, status, _headers, _body} ->
+            refute "Expected 200, got #{status}"
 
-        {:error, error} ->
-          refute "Expected response, got #{inspect(error)}"
+          {:error, error} ->
+            refute "Expected response, got #{inspect(error)}"
+        end
       end
-    end
 
-    test "post/4" do
-      case Mixpanel.HTTP.Hackney.post("https://localhost:40010/post_endpoint", "body", [], [
-             :insecure
-           ]) do
-        {:ok, 200, _headers, body} ->
-          assert Jason.decode!(body)
-                 ~> %{
-                   "body" => "body",
-                   "headers" =>
-                     in_any_order([
-                       ["user-agent", "hackney/1.20.1"],
-                       ["host", "localhost:40010"],
-                       ["content-length", "4"],
-                       ["content-type", "application/octet-stream"]
-                     ]),
-                   "http_protocol" => string(starts_with: "HTTP"),
-                   "method" => "POST",
-                   "query_params" => map(size: 0),
-                   "request_url" => string(ends_with: "/post_endpoint")
-                 }
+      test "#{adapter}.post/4" do
+        case unquote(adapter).post(
+               "https://localhost:40010/post_endpoint",
+               "body",
+               [
+                 {"Content-Type", "application/x-www-form-urlencoded"}
+               ],
+               insecure: true
+             ) do
+          {:ok, 200, _headers, body} ->
+            assert Jason.decode!(body)
+                   ~> %{
+                     "body" => "body",
+                     "headers" =>
+                       list(min: 1, elements: list(min: 2, max: 2, elements: string())),
+                     "http_protocol" => string(starts_with: "HTTP"),
+                     "method" => "POST",
+                     "query_params" => map(size: 0),
+                     "request_url" => string(ends_with: "/post_endpoint")
+                   }
 
-        {:ok, status, _headers, _body} ->
-          refute "Expected 200, got #{status}"
+          {:ok, status, _headers, _body} ->
+            refute "Expected 200, got #{status}"
 
-        {:error, error} ->
-          refute "Expected 200, got #{inspect(error)}"
+          {:error, error} ->
+            refute "Expected 200, got #{inspect(error)}"
+        end
       end
     end
   end
